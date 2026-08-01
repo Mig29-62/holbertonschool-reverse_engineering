ef5057f61915fb0bd65b6b24c799ec21442d9e19
mig29-62 <12584@holbertonstudents.com>
2026-08-01 04:52:42 -0400

    holberton

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -2,6 +2,7 @@
 source ./messages.sh
 file_name=$1
 magic_number=$(readelf -h $1 | grep Magic | awk '{print $2,$3,$4,$5}')
+class=$(readelf -h $1 | grep Class | awk '{print $2}')
 byte_order=$(readelf -h $1 | grep Data | awk '{print $2,$3,$4,$5}')
 entry_point_address=$(readelf -h $1 | grep Entry | awk '{print $4}')
 display_elf_header_info
